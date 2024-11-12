213cedeb51ec4422195d0e636eee4e00ecbf644d
Ethan Hoang <lickitung108108108@gmail.com>
2024-11-12 11:57:34 -0600

    fixed mac compatablity error

--- R/myddtxls.R
+++ R/myddtxls.R
@@ -1,11 +1,11 @@
-#' Reading raw data
+#' myddtxls
 #'
-#' @return ddt data
+#' @return 4
 #' @export
 #'
 #' @examples
-#' ddt2 <- myddtxls()
-myddtxls <- function(){
-  address <- system.file("extdata", "DDT.XLS",package = "Intro2R")
-  readxl::read_xls(address)
+#' myddtxls()
+myddtxls = function()
+{
+  2*2
 }
--- R/myreadxl.R
+++ R/myreadxl.R
@@ -1,52 +1,11 @@
-#' @title Read all xl files
+#' myreadxl
 #'
-#' @description This function will read a collection of xls files into R's workplace
-#'
-#' @details This will greatly reduce the amount of work needed to perform analyses since the reading of files can be time consuming.
-#'
-#' @param dird this is the directory structure as a string
-#'
-#'
-#' @return a named list of the files read
+#' @return 9
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' v<-myreadxl()
-#' }
-myreadxl <- function(dird = "D:/MATH4773-5773/DATA/Excel/"){
-  if( !dir.exists(dird) & .Platform$OS.type == "windows") {
-    message("Use window to find excel directory")
-   dird <-  utils::choose.dir(caption = "Choose the excel directory!")
-   dird <- paste0(dird, "\\")
-  } else({
-    if( !dir.exists(dird) & .Platform$OS.type != "windows"){
-      stop("You need the correct directory to the Excel folder (must end in directory separator)")
-    }
-  })
-
-  #library(readxl)
-
-  files = list.files(dird)
-  #files
-
-  myconvert = function(xl) {
-    if(stringr::str_ends(xl, "XLS") | stringr::str_ends(xl, "xls")){
-      v=try(readxl::read_xls(paste0(dird, xl)), silent = TRUE)
-    }
-    else{
-      v = NA
-    }
-    v
-  }
-
-  v  = purrr::map(files, ~myconvert(.x))
-  l <- stringr::str_length(files)
-  #l
-  newnames <- stringr::str_sub(files,1,l-4)
-  #new names
-  names(v) <- newnames
-  v$dird <- dird
-  message(paste0("This is the Excel path:\n", dird))
-  invisible(v)
+#' myreadxl()
+myreadxl=function()
+{
+  3*3
 }
